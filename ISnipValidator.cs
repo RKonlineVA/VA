c44854750ee792fe74b23775234d858d904c0447
RKonline <randy.kirksey@va.gov>
2026-07-01 18:17:13 -0400

    tt

--- ISnipValidator.cs
+++
@@ -1,9 +0,0 @@
-csharp src\EDI275AttachmentParser\Services\ISnipValidator.cs
-namespace EDI275AttachmentParser.Services
-{
-    public interface ISnipValidator
-    {
-        bool IsValid(string? snip);
-        string Normalize(string? snip);
-    }
-}
